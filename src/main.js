6e03932db22c30167851bd96ab610cb1fd56dab2
Toffee <toffee@desktop-ovgath5.home>
2020-07-31 13:41:13 +0100

    should be working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-/*
-    Much javascript here.
-*/
-
-console.log('hello')
+function validateAddr() {
+    var address = document.forms["testnetAddrForm"]["testnetAddr"].value;
+  
+    console.log(address);
+  
+    if (address.charAt(0) == "m" || address.charAt(0) == "n") {
+      return True;
+    } else {
+      return False;
+    }
+  
+  }
